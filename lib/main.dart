4c368ba0f42cdaea55ae0dd1360dad048e24b0ca
Senad Hajric <senadhajric3@gmail.com>
2024-12-11 13:26:28 +0100

    Hard coded LoginPage with User

--- lib/main.dart
+++ lib/main.dart
@@ -14,9 +14,69 @@ class MainApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Login Example!'),
+      home: LoginPage(),
+    );
+  }
+}
+
+class LoginPage extends StatefulWidget {
+  const LoginPage({super.key});
+
+  _LoginPageState createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  String _message = "";
+
+  void _loginUser() {
+    const username = "Senad";
+    const password = "Hallo";
+
+    setState(() {
+      _message = "User '$username' is Loged in";
+    });
+  }
+
+  void _logout() {
+    setState(() {
+      _message = "User succesfully loged out";
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text(
+          "Login Dude..",
+          style: TextStyle(
+            fontWeight: FontWeight.bold,
+            color: Colors.orange,
+          ),
+        ),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(24.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            TextField(
+              decoration: const InputDecoration(
+                border: OutlineInputBorder(),
+              ),
+              controller: TextEditingController(text: _message),
+            ),
+            const SizedBox(height: 16),
+            ElevatedButton(
+              onPressed: _loginUser,
+              child: const Text("Log TF in"),
+            ),
+            const SizedBox(height: 16),
+            ElevatedButton(
+              onPressed: _logout,
+              child: const Text("Get TF out here"),
+            ),
+          ],
         ),
       ),
     );
